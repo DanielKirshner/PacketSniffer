fc3b3fb66ca196d9e1d09f56dd12d8f9d2d849ba
DanielKirshner <danielkirshner7878@gmail.com>
2022-12-10 15:51:36 +0200

    Created main.c

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <net/ethernet.h>
+#include <netinet/ip.h>
+#include <netinet/if_ether.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+int main(int argc, char* argv[])
+{
+    int errorCode = 0;
+    ssize_t data_size;
+    
+
+
+    cleanup:
+        return errorCode;
+}
