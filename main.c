faf93528a6f47f08baab1b7cde604ca0337e86ef
DanielKirshner <danielkirshner7878@gmail.com>
2022-12-10 16:47:35 +0200

    Socket error handling

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <unistd.h>
 #include <string.h>
 #include <net/ethernet.h>
@@ -7,13 +8,36 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
+#define PACKET_BUFFER_SIZE 65536 // for MTU=1500 bytes 
+
 int main(int argc, char* argv[])
 {
-    int errorCode = 0;
+    int error_code = 0;
     ssize_t data_size;
+    uint8_t packet_buffer[PACKET_BUFFER_SIZE];
+
+    if(argc != 2)
+    {
+        printf("Usage %s [InterfaceName]\n", argv[0]);
+        error_code = 1;
+        goto cleanup;
+    }
+
+    const char* interface_name = argv[1];
+    int raw_socket = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+    if(raw_socket == -1)
+    {
+        perror("socket");
+        error_code = 2;
+        goto cleanup;
+    }
+
     
 
-
-    cleanup:
-        return errorCode;
+cleanup:
+    if(raw_socket != -1 && close(raw_socket) == -1)
+    {
+        perror("close");
+    }
+    return error_code;
 }
